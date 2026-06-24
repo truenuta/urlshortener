1631f0ff149ff8ecd425adf9a4bd9f5d0e00155a
trukhova_aa <trukhova.aa@magnit.ru>
2026-06-24 14:34:14 +0300

    add increment1

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,48 @@
 package main
 
-func main() {}
+import (
+	"io"
+	"net/http"
+	"strings"
+
+	"github.com/google/uuid"
+)
+
+var storage = make(map[string]string)
+
+func shorten(body string) string {
+	id := uuid.New().String() // заменить на нормальный метод
+	storage[id] = body
+	return id
+}
+
+func ShortenRequest(response http.ResponseWriter, request *http.Request) {
+	if request.Method != http.MethodPost && request.Method != http.MethodGet {
+		response.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+	if request.Method == http.MethodPost {
+		body, _ := io.ReadAll(request.Body)
+		defer request.Body.Close()
+		url := shorten(string(body))
+		response.Header().Set("content-type", "text/plain")
+		response.WriteHeader(http.StatusCreated)
+		response.Write([]byte(url))
+	}
+	if request.Method == http.MethodGet {
+		id := strings.TrimPrefix(request.URL.Path, "/")
+		original_url := storage[id]
+		response.Header().Set("Location", original_url)
+		response.WriteHeader(http.StatusTemporaryRedirect)
+	}
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", ShortenRequest)
+	err := http.ListenAndServe(":8080", mux)
+	if err != nil {
+		panic(err)
+	}
+
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/truenuta/urlshortener
+
+go 1.26.4
+
+require github.com/google/uuid v1.6.0 // indirect
