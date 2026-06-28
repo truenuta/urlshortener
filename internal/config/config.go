1af86ee6523b5d3d548f96eb0040c4700a3765b7
trukhova_aa <trukhova.aa@magnit.ru>
2026-06-28 16:34:02 +0300

    fix default port

--- internal/config/config.go
+++ internal/config/config.go
@@ -12,7 +12,7 @@ type Config struct {
 func ParseFlags() *Config {
 	var address string
 	var baseShortURLAddress string
-	flag.StringVar(&address, "a", "localhost:8888", "адрес запуска HTTP-сервера")
+	flag.StringVar(&address, "a", "localhost:8080", "адрес запуска HTTP-сервера")
 	flag.StringVar(&baseShortURLAddress, "b", "http://localhost:8080", "базовый адрес результирующего сокращённого URL ")
 	flag.Parse()
 	return &Config{
