6bba47c8b6de5c2492cc93d1ef983b02247e8091
trukhova_aa <trukhova.aa@magnit.ru>
2026-07-18 14:38:50 +0300

    add iter6 logger

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -4,9 +4,12 @@ import (
 	"log"
 	"net/http"
 
+	"go.uber.org/zap"
+
 	"github.com/go-chi/chi/v5"
 	"github.com/truenuta/urlshortener/internal/config"
 	"github.com/truenuta/urlshortener/internal/handler"
+	"github.com/truenuta/urlshortener/internal/logger"
 	"github.com/truenuta/urlshortener/internal/repository"
 	"github.com/truenuta/urlshortener/internal/service"
 )
@@ -14,17 +17,23 @@ import (
 func main() {
 
 	cfg := config.NewConfig()
+	zapLogger, err := zap.NewDevelopment()
+	if err != nil {
+		panic(err)
+	}
+
 	repository := repository.NewStorage()
 	service := service.NewURLServiсe(repository)
 	h := handler.NewHandler(cfg.BaseShortURLAddress, service)
 	r := chi.NewRouter()
+	r.Use(logger.RequestLogger(zapLogger))
 
 	r.Post("/", h.ShortenURL)
 	r.Get("/{id}", h.GetOriginalURL)
 
-	err := http.ListenAndServe(cfg.Address, r)
-	if err != nil {
-		log.Fatal(err)
+	LaSerr := http.ListenAndServe(cfg.Address, r)
+	if LaSerr != nil {
+		log.Fatal(LaSerr)
 	}
 
 }
---
+++ internal/logger/logger.go
@@ -0,0 +1,57 @@
+package logger
+
+import (
+	"net/http"
+	"time"
+
+	"go.uber.org/zap"
+)
+
+type responseData struct {
+	status int
+	size   int
+}
+
+type loggingResponseWriter struct {
+	http.ResponseWriter
+	responseData *responseData
+}
+
+func (r *loggingResponseWriter) Write(b []byte) (int, error) {
+	size, err := r.ResponseWriter.Write(b)
+	r.responseData.size += size
+	return size, err
+}
+
+func (r *loggingResponseWriter) WriteHeader(statusCode int) {
+	r.ResponseWriter.WriteHeader(statusCode)
+	r.responseData.status = statusCode
+}
+func RequestLogger(logger *zap.Logger) func(http.Handler) http.Handler {
+	return func(h http.Handler) http.Handler {
+
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			responseData := &responseData{
+				status: 0,
+				size:   0,
+			}
+			lw := loggingResponseWriter{
+				ResponseWriter: w,
+				responseData:   responseData,
+			}
+			start := time.Now()
+			url := r.RequestURI
+			method := r.Method
+			h.ServeHTTP(&lw, r)
+			duration := time.Since(start)
+
+			logger.Info("Fetch URL",
+				zap.String("url", url),
+				zap.String("method", method),
+				zap.Duration("duration", duration),
+				zap.Int("status", responseData.status),
+				zap.Int("size", responseData.size),
+			)
+		})
+	}
+}
